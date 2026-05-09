1977f74ab0964245852e2738f4ba9112e9cc515f
killioiden <derek.verduijn@proton.me>
2026-05-09 15:32:46 +0200

    commit and push that

--- flake.nix
+++ flake.nix
@@ -2,14 +2,17 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-25.11";
   };
 
-  outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
+  outputs = { self, nixpkgs }@inputs:
+  {
+    nixosConfigurations.six = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      specialArgs = { inherit inputs; };
+      modules = [
+        ./systems/six
+      ];
+    };
   };
 }
