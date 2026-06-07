721f6437087976ddbc492119d053d065d1d463b0
killioiden <derek.verduijn@proton.me>
2026-06-07 23:12:39 +0200

    Hyprland autocomplete werks

--- modules/home-manager/hyprland.nix
+++ modules/home-manager/hyprland.nix
@@ -1,21 +1,8 @@
-{ pkgs, home, ... }:
+{ ... }:
 {
   programs.hyprland = {
     enable = true;
     withUWSM = true;
     xwayland.enable = true;
-  };
-
-  home.file.".config/hypr/.luarc.json" = {
-    text = builtins.toJSON {
-      workspace = {
-        library = [
-          "${pkgs.hyprland}/share/hypr/stubs"
-        ];
-      };
-      diagnostics = {
-        globals = [ "hl" ];
-      };
-    };
   };
 }
--- users/killioiden/default.nix
+++ users/killioiden/default.nix
@@ -21,8 +21,22 @@
     extraSpecialArgs = { inherit inputs; };
     useGlobalPkgs = true;
     useUserPackages = true;
-    users.killioiden = {
-      home = ../../systems/thirtysix/home.nix;
+    users.killioiden.home = {
+      packages = [ ];
+      stateVersion = "25.11";
+      file.".config/hypr/.luarc.json" = {
+        text = builtins.toJSON {
+          workspace = {
+            library = [
+              "${pkgs.hyprland}/share/hypr/stubs"
+            ];
+          };
+          diagnostics = {
+            globals = [ "hl" ];
+          };
+        };
+      };
     };
+
   };
 }
