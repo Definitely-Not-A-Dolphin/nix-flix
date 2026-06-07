cea78c8682b75a04047eefeed52af85c94b98069
killioiden <derek.verduijn@proton.me>
2026-06-07 23:46:55 +0200

    Update default.nix

--- systems/thirtysix/default.nix
+++ systems/thirtysix/default.nix
@@ -23,6 +23,7 @@
 
     # WM
     hyprland
+    hyprshot
     noctalia-shell
     wayland
 
@@ -43,7 +44,7 @@
 
   programs.nix-ld = {
     enable = true;
-    libraries = with pkgs; [
+    libraries = [
       # Add any missing dynamic libraries for unpackaged programs
       # here, NOT in environment.systemPackages
     ];
