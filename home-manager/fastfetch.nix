48052a5021e453c14ad4559c64b885758f7c29b0
killioiden <derek.verduijn@proton.me>
2026-08-20 16:17:01 +0200

    Update fastfetch.nix

--- home-manager/fastfetch.nix
+++ home-manager/fastfetch.nix
@@ -37,27 +37,27 @@
             }
             {
               type = "custom";
-              format = "  ${codes.grey}{#1}system{#0}   ${codes.dark-grey}= ${codes.grey}{{";
+              format = "  ${codes.grey}${codes.blue}system{#0}   ${codes.dark-grey}= ${codes.grey}{{";
             }
             {
               type = "os";
               key = "    ${codes.blue}os       {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{name} {version} ({codename})\"{#0}${codes.grey};";
+              format = "${codes.green}${codes.blue}\"{name} {version} ({codename})\"{#0}${codes.grey};";
             }
             {
               type = "os";
               key = "    ${codes.blue}arch     {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{arch}\"{#0}${codes.grey};";
+              format = "${codes.green}${codes.blue}\"{arch}\"{#0}${codes.grey};";
             }
             {
               type = "locale";
               key = "    ${codes.blue}locale   {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{result}\"{#0}${codes.grey};";
+              format = "${codes.green}${codes.blue}\"{result}\"{#0}${codes.grey};";
             }
             {
               type = "uptime";
               key = "    ${codes.blue}uptime   {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{days} days, {hours} hours, {minutes} mins\"{#0}${codes.grey};";
+              format = "${codes.green}${codes.blue}\"{days} days, {hours} hours, {minutes} mins\"{#0}${codes.grey};";
             }
             {
               type = "custom";
@@ -66,28 +66,28 @@
             "break"
             {
               type = "custom";
-              format = "  ${codes.grey}{#1}hardware{#0} ${codes.dark-grey}= ${codes.grey}{{";
+              format = "  ${codes.grey}${codes.blue}hardware{#0} ${codes.dark-grey}= ${codes.grey}{{";
             }
             {
               type = "cpu";
               key = "    ${codes.blue}cpu      {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{name} ({cores-logical}) @ {freq-max}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{name} ({cores-logical}) @ {freq-max}\"{#0}${codes.dark-grey};";
             }
             {
               type = "gpu";
               key = "    ${codes.blue}gpu      {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{name}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{name}\"{#0}${codes.dark-grey};";
             }
             {
               type = "memory";
               key = "    ${codes.blue}ram      {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{used<10} / {total>9}\"{#0}${codes.dark-grey}; #{percentage}";
+              format = "${codes.green}${codes.blue}\"{used<10} / {total>9}\"{#0}${codes.dark-grey}; #{percentage}";
             }
             {
               type = "disk";
               folders = "/";
               key = "    ${codes.blue}disk0    {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{size-used<10} / {size-total>9}\"{#0}${codes.dark-grey}; #{size-percentage}";
+              format = "${codes.green}${codes.blue}\"{size-used<10} / {size-total>9}\"{#0}${codes.dark-grey}; #{size-percentage}";
             }
             {
               type = "custom";
@@ -96,27 +96,27 @@
             "break"
             {
               type = "custom";
-              format = "  ${codes.grey}{#1}desktop{#0}  ${codes.dark-grey}= ${codes.grey}{{";
+              format = "  ${codes.grey}${codes.blue}desktop{#0}  ${codes.dark-grey}= ${codes.grey}{{";
             }
             {
               type = "de";
               key = "    ${codes.blue}de       {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{pretty-name} {version}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{pretty-name} {version}\"{#0}${codes.dark-grey};";
             }
             {
               type = "wm";
               key = "    ${codes.blue}wm       {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{pretty-name} ({protocol-name})\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{pretty-name} ({protocol-name})\"{#0}${codes.dark-grey};";
             }
             {
               type = "display";
               key = "    ${codes.blue}display  {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{width}x{height} @ {refresh-rate}Hz\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{width}x{height} @ {refresh-rate}Hz\"{#0}${codes.dark-grey};";
             }
             {
               type = "localip";
               key = "    ${codes.blue}localIp  {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{ipv4}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{ipv4}\"{#0}${codes.dark-grey};";
             }
             {
               type = "custom";
@@ -125,22 +125,22 @@
             "break"
             {
               type = "custom";
-              format = "  ${codes.grey}{#1}terminal{#0} ${codes.dark-grey}= ${codes.grey}{{";
+              format = "  ${codes.grey}${codes.blue}terminal{#0} ${codes.dark-grey}= ${codes.grey}{{";
             }
             {
               type = "shell";
               key = "    ${codes.blue}shell    {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{pretty-name} {version}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{pretty-name} {version}\"{#0}${codes.dark-grey};";
             }
             {
               type = "terminal";
               key = "    ${codes.blue}term     {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{pretty-name}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{pretty-name}\"{#0}${codes.dark-grey};";
             }
             {
               type = "packages";
               key = "    ${codes.blue}packages {#0}${codes.dark-grey}= ";
-              format = "${codes.dark-grey}[ ${codes.green}{#1}\"{nix-system} nix-system\" \"{nix-user} nix-user\"{#0}${codes.dark-grey} ];";
+              format = "${codes.dark-grey}[ ${codes.green}${codes.blue}\"{nix-system} nix-system\" \"{nix-user} nix-user\"{#0}${codes.dark-grey} ];";
             }
             {
               type = "custom";
@@ -149,31 +149,31 @@
             "break"
             {
               type = "custom";
-              format = "  ${codes.grey}{#1}dev{#0}      ${codes.dark-grey}= ${codes.grey}{{";
+              format = "  ${codes.grey}${codes.blue}dev{#0}      ${codes.dark-grey}= ${codes.grey}{{";
             }
             {
               type = "command";
               text = "deno -v | sed 's/deno //'";
               key = "    ${codes.blue}deno     {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{result}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{result}\"{#0}${codes.dark-grey};";
             }
             {
               type = "command";
               text = "rustc --version | cut -d' ' -f2";
               key = "    ${codes.blue}rust     {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{result}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{result}\"{#0}${codes.dark-grey};";
             }
             {
               type = "command";
               text = "nix --version | cut -d' ' -f3";
               key = "    ${codes.blue}nix      {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{result}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{result}\"{#0}${codes.dark-grey};";
             }
             {
               type = "command";
               text = "git --version | cut -d' ' -f3";
               key = "    ${codes.blue}git      {#0}${codes.dark-grey}= ";
-              format = "${codes.green}{#1}\"{result}\"{#0}${codes.dark-grey};";
+              format = "${codes.green}${codes.blue}\"{result}\"{#0}${codes.dark-grey};";
             }
             {
               type = "custom";
